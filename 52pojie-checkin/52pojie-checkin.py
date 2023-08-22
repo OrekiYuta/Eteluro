ec7181ae3cb05c82affdfdd6dcf022f9869886aa
Orekiyuta <orekiyuta@gmail.com>
2023-08-23 00:48:54 +0800

    fix: handle update chromedriver version

--- 52pojie-checkin/52pojie-checkin.py
+++ 52pojie-checkin/52pojie-checkin.py
@@ -1,14 +1,18 @@
 import time
 
 from selenium import webdriver
-
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
 
 def main():
-    option = webdriver.ChromeOptions()
-    option.add_argument("--user-data-dir=" + r"C:/Users/OrekiYuta/AppData/Local/Google/Chrome/User Data/")
-    option.add_experimental_option('detach', True)
-    option.add_experimental_option("excludeSwitches", ["enable-logging"])
-    driver = webdriver.Chrome(options=option)  # open chrome
+
+    chromedriver_autoinstaller.install(cwd=True)
+    chrome_options = Options()
+
+    chrome_options.add_argument("--user-data-dir=" + r"C:/Users/OrekiYuta/AppData/Local/Google/Chrome/User Data/")
+    chrome_options.add_experimental_option('detach', True)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(options=chrome_options)  # open chrome
 
     url_homepage: str = 'https://www.52pojie.cn/forum.php'
     url_checkin: str = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
